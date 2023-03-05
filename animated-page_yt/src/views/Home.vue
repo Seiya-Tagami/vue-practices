d93748b651111d930cec838e2f85edb9173e759b
seiya_tagami <bepozurapu274@gmail.com>
2023-03-05 16:09:18 +0900

    modify folder name

---
+++ animated-page_yt/src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, enim.</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam veniam qui tempora unde repellat vero atque! Quod tenetur assumenda odit maxime quasi dicta? Dolores, nesciunt.
+    </p>
+  </main>
+</template>
