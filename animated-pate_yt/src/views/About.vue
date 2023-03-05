4effcb6565f56bde7441b6f2876aba3880aa3d37
seiya_tagami <bepozurapu274@gmail.com>
2023-03-05 16:07:56 +0900

    done

---
+++ animated-pate_yt/src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <img src="https://picsum.photos/800/300" alt="Placeholder" />
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione distinctio veniam nobis enim porro. Repellendus.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, incidunt deleniti omnis et error impedit?</p>
+  </main>
+</template>
